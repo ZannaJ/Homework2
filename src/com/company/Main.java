88c3fad14bcb7d7a1d35048500972eba6bff2fca
ZannaJ <zhanna.jelatanceva@gmail.com>
2021-06-29 21:51:11 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String five = "5";
+	int five5 = Integer.parseInt(five);
+
+	String eight = "8";
+	int eight8 = Integer.parseInt(eight);
+
+	System.out.println((five5) + (eight8));
+
+    }
+}
